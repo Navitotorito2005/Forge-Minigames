e087f72e3501ddfed262096300425183d2ea0c39
Navi <148604490+Navitotorito2005@users.noreply.github.com>
2026-02-11 15:52:57 -0600

    Add files via upload

---
+++ lucydodgeball/modinfo.lua
@@ -0,0 +1,24 @@
+name = "Lucy Dodgeball"
+author = "Navi"
+version = "0.1"
+forumthread = ""
+
+dst_compatible = true
+gorge_compatible = false
+forge_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+
+client_only_mod = true
+server_only_mod = false
+all_clients_require_mod = false
+server_filter_tags = {}
+
+priority = -999
+api_version_dst = 10
+
+icon = "modicon.tex"
+icon_atlas = "modicon.xml"
+
+description = "This mod contains a pack of console commands to host Lucy Dodgeball in The Forge!"
